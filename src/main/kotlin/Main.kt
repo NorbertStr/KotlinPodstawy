8d76c51b109d87efb124398c3159cf523542ac2f
NorbertStr <nstrzeszyna@gmail.com>
2022-10-20 20:48:03 +0200

    zmienne

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,17 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    var liczba = 1
+    var liczba2 = 2
+
+    var wynik = liczba + liczba2
+
+    wynik += liczba  //jest to skrót od: wynik = wynik + liczba
+    wynik *= liczba
+    wynik -= liczba
+    wynik /= liczba
+
+    //Operator modulo:
+    var rezultat = 10%3 // modulo w tym przypadku to jeden, bo jest to wynik reszty z dzielenia
+
+
 }
