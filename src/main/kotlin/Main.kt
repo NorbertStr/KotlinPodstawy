14f7b5e7a76d54bf83582c1353e3926bfdba4e0b
NorbertStr <nstrzeszyna@gmail.com>
2022-10-20 20:33:43 +0200

    zmienne

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,42 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    /**
+     * Typy zmiennych:
+     * Całkowite: Byte, Short, Int, Long
+     * Zmiennoprzecinkowe: Float, Double
+     * Znakowe: Char, String
+     * Wartości logiczne: Boolean
+     */
+
+    /**
+     * Pełen zapis zmiennej wygląda przykładowo w ten sposób:
+     * var nazwaZmiennej: TypZmiennej = wartośćZmiennej
+     */
+
+    //skrótowy zapis wartości zmiennych:
+
+    //Int jest zawsze domyślną wartością dla zmiennych liczbowych
+    var liczbaInt = 1
+    var liczbaLong = 2L
+
+    //Byte i Short nie mają skrótowego zapisu
+    var liczbaByte: Byte = 1
+    var liczbaShort: Short = 3
+
+    var liczbaFloat = 2f
+    var liczbaDouble = 2.0
+
+    var znakChar = 'a'
+    var ciagZnakowString = "adfasf"
+
+    var wartosciLogiczne = true // lub folse
+
+    //Do inicjalizacji stałych używamy "val"
+    val stalaInt = 12
+
+
+
+
+
+
 }
