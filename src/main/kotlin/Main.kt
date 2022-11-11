ee2806a97d70e6a9fed4b73f4742b03a24887b49
NorbertStr <nstrzeszyna@gmail.com>
2022-11-11 21:47:19 +0100

    sealed class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,11 @@
+
+sealed class Response{
+    class Success(val data: String): Response()
+    class Failure(val errorCode: Int): Response()
+    class Loading(val isLoading: Boolean): Response()
+
+}
+
 fun main(args: Array<String>) {
 
 
