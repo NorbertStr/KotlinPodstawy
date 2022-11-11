9a32fe193fc41274ced15956105e154798c1bec9
NorbertStr <nstrzeszyna@gmail.com>
2022-11-11 19:43:43 +0100

    Object i singletone

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,42 @@
+ import java.awt.event.MouseEvent
+import java.awt.event.MouseListener
+import javax.swing.JFrame
+
+object mySingleton{
+
+}
+
+class MyClass {
+    companion object{
+        const val  MY_INT = 10
+    }
+}
 fun main(args: Array<String>) {
 
+    val jFrame = JFrame()
+//    dodanie nasłuchiwania na zdarzenia myszki
+    jFrame.addMouseListener(object: MouseListener{
+        override fun mouseClicked(e: MouseEvent?) {
+            println("Click")
+        }
 
+        override fun mousePressed(e: MouseEvent?) {
+            TODO("Not yet implemented")
+        }
+
+        override fun mouseReleased(e: MouseEvent?) {
+            TODO("Not yet implemented")
+        }
+
+        override fun mouseEntered(e: MouseEvent?) {
+            TODO("Not yet implemented")
+        }
+
+        override fun mouseExited(e: MouseEvent?) {
+            TODO("Not yet implemented")
+        }
+
+    })
+
+    jFrame.isVisible = true
 }
