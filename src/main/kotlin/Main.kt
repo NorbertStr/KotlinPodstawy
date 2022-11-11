d2c9a3acefdb66e892a0c4abdc4e68c5d8bb34b3
NorbertStr <nstrzeszyna@gmail.com>
2022-11-11 19:56:57 +0100

    Enum - typ wyliczeniowy

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,19 @@
+
+/*
+    Enum, czyli tryb wyliczeniowy jest to taka konstrukcja, która pozwala nam wyliczać konkretne elementy.
+ */
+enum class Orientation{
+    PORTRAIT, LANDSCAPE, REVERSED_PORTRAIT, REVERSED_LANDSCAPE
+}
+fun setOrientation(orientation: Orientation){
+    when (orientation){
+        Orientation.PORTRAIT -> {}
+        Orientation.LANDSCAPE -> {}
+        Orientation.REVERSED_PORTRAIT -> {}
+        Orientation.REVERSED_LANDSCAPE -> {}
+    }
+}
+
 fun main(args: Array<String>) {
 
 
