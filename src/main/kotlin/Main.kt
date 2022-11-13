d1fc0bf3bed86d57f67b216c2a63a0b31f7f8959
NorbertStr <nstrzeszyna@gmail.com>
2022-11-13 18:11:57 +0100

    scope function "let"

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,6 +13,9 @@ fun main(args: Array<String>) {
     person.surname = "Kowalski"
     person.age = 33
 
+    /*
+            APPLY:
+     */
 //    scope functjon apply sprawia, że możemy zrobić to w krótszy sposób, nie pisząc cały czas nazwy obiektu:
     person.apply {
         this.name = "Kacper"
@@ -24,5 +27,24 @@ fun main(args: Array<String>) {
         name = "Marian"
         surname = "Rak"
         age = 59
+    }
+
+    /*
+            LET:
+     */
+
+//    "let" pozwala nam wykonać jakiś blok instrukcji i zwrucić ostatnią wartość
+    val nameSize = person.name.let { it.length } // W kontekście klamer "it" odnosi się do tej propercji
+//    w tym przypadku zwruci nam długość stringa zmiennej name
+    println(nameSize)
+
+//    inną rzeczą, do której możemy użyć "let" to sprawdzenie null:
+    var coMozeBycNullem: Int? = 4
+
+//    jeśli zapiszemy nazwę naszej propercji ze znakiem zapytania, to blok instrukcji wykona się tylko, jeżeli ta propercja
+//    nie będzie nullem
+    coMozeBycNullem?.let { nieNull ->
+        println("Wartość równa się: $nieNull")
+//    Jeśli wartość byłaby nullem to instrukcja nie wykonałaby się.
     }
 }
