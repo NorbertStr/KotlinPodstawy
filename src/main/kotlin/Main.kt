afbad636cefffa95adc304ec58dcf37fcc20d329
NorbertStr <nstrzeszyna@gmail.com>
2022-11-15 20:36:59 +0100

    infix function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,12 @@
+/*
+    Infinx functions nie są zbyt użyteczne pozwalają jedynie na ładniejszy zapis.
+ */
+infix fun String.concate(element: String) = this + element
+
 fun main(args: Array<String>) {
 
-
+    val result1 = "hej".concate(" Wojtek")
+    val result2 = "hej" concate " Wojtek"
+    println(result1)
+    println(result2)
 }
